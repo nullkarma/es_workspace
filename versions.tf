acf3c29f2f72ae39943ce72d7e97c508cce0ba17
Daniel Chamot <daniel@nullkarma.com>
2023-04-03 12:19:57 +0200

    remove unused providers & updat tech user example

--- versions.tf
+++ versions.tf
@@ -8,13 +8,5 @@ terraform {
       source  = "disaster37/kibana"
       version = "8.5.2"
     }
-    restapi = {
-      source = "Mastercard/restapi"
-      version = "1.18.0"
-    }
-    curl = {
-      source  = "marcofranssen/curl"
-      version = "0.2.2"
-    }
   }
 }
